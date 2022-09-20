aa139994f6c6053260e320f8af78d3cfbc6fee4e
Bartosz <bartosz@iMac-Bartosz.local>
2022-09-20 12:30:08 +0200

    add filter's

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 
 import NewExpense from './components/NewExpense/NewExpense';
 import Expenses from './components/Expenses/Expenses';
-import ExpensesFilter from './components/Expenses/ExpensesFilter';
 
 const DUMMY_EXPENSES = [
   {
@@ -29,7 +28,7 @@ const DUMMY_EXPENSES = [
 const App = () => {
   const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense => {
+  const addExpenseHandler = (expense) => {
     setExpenses(prevExpenses => {
       return [expense, ...prevExpenses];
     });
